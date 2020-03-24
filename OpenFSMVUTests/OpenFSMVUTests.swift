93341319fd5513bb110830e3362417599c2574e6
Mert <mert.agcakoyun@stu.fsm.edu.tr>
2020-03-24 03:27:48 +0300

    Starting Project.. -Login Page Design

---
+++ OpenFSMVUTests/OpenFSMVUTests.swift
@@ -0,0 +1,34 @@
+//
+//  OpenFSMVUTests.swift
+//  OpenFSMVUTests
+//
+//  Created by Mert Agcakoyun on 23.03.2020.
+//  Copyright © 2020 Mert Agcakoyun. All rights reserved.
+//
+
+import XCTest
+@testable import OpenFSMVU
+
+class OpenFSMVUTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
